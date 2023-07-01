254cdcc52312d49e4cae062ffaaeecbc6d774dee
PRANAV K DILEEP <91619846+pranavkdileep@users.noreply.github.com>
2023-07-01 09:35:21 +0000

    updatesd

---
+++ eg.py
@@ -0,0 +1,24 @@
+import json
+import glob
+from gradio_client import Client
+import os
+
+def chat(question):
+    client = Client("http://127.0.0.1:7860/")
+    result = client.predict(
+        question,  # Input text
+        api_name="/bot"
+    )
+
+    # Find the latest JSON file in the directory
+    json_files = glob.glob("/tmp/gradio/tmp*.json")
+    latest_json_file = max(json_files, key=lambda x: os.path.getmtime(x))
+
+    # Print the contents of the JSON file
+    with open(latest_json_file) as file:
+        json_data = json.load(file)
+        answer = json_data[0][1]
+        print(answer)
+        return answer
+
+chat("what is your name?")
